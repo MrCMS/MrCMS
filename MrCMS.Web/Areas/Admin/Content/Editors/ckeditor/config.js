770a83e647e90e784820c92b9f4f76d62388ac8c
thought <will.anderson@thought.co.uk>
2012-12-19 17:03:27 +0000

    start adding new editor

--- MrCMS.Web/Areas/Admin/Content/Editors/ckeditor/config.js
+++ MrCMS.Web/Areas/Admin/Content/Editors/ckeditor/config.js
@@ -1,15 +1,41 @@
-﻿/*
-Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
+/**
+ * @license Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
 
-CKEDITOR.editorConfig = function(config) {
-    // Define changes to default configuration here. For example:
-    // config.language = 'fr';
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
     // config.uiColor = '#AADC6E';
     config.filebrowserImageBrowseUrl = '#';
     config.height = '300px';
-    
+
     config.contentsCss = '/Content/Styles/style.css';
 
+    config.toolbar = 'Full';
+
+    config.toolbar_Full =
+    [
+        { name: 'document', items: ['Source', '-', 'Templates'] },
+        { name: 'clipboard', items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo'] },
+        { name: 'editing', items: ['Find', 'Replace', '-', 'SelectAll', '-', 'SpellChecker', 'Scayt'] },
+        { name: 'basicstyles', items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat'] },
+        { name: 'styles', items: ['Format', 'FontSize'] },
+        { name: 'colors', items: ['TextColor', 'BGColor'] },
+        { name: 'tools', items: ['Maximize', 'ShowBlocks'] },
+        {
+            name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv',
+            '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl']
+        },
+        { name: 'links', items: ['Link', 'Unlink', 'Anchor'] },
+        { name: 'insert', items: ['Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe'] }
+        
+    ];
+
+    config.toolbar_Basic =
+    [
+        ['Templates', 'Bold', 'Italic', 'RemoveFormat', 'Outdent', 'Indent', '-', 'Blockquote', 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink', '-', 'Flash', 'Table', 'HorizontalRule', 'Format']
+    ];
+
+    
 };
