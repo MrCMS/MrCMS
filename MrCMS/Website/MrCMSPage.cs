038088bfb9912b64cc49968055074472142975ca
gmstr <gary.m.strong@gmail.com>
2013-01-04 13:28:36 +0000

    Adding custom page type to allow access to settings in the page

---
+++ MrCMS/Website/MrCMSPage.cs
@@ -0,0 +1,32 @@
+﻿using MrCMS.Services;
+using MrCMS.Settings;
+
+namespace MrCMS.Website
+{
+    public abstract class MrCMSPage<TModel> : System.Web.Mvc.WebViewPage<TModel>
+    {
+        private IConfigurationProvider _configurationProvider;
+        private ISiteService _siteService;
+
+        public T Settings<T>() where T : ISettings, new()
+        {
+            return _configurationProvider.GetSettings<T>(_siteService.GetCurrentSite());
+        }
+
+        public override void InitHelpers()
+        {
+            base.InitHelpers();
+
+            if (MrCMSApplication.DatabaseIsInstalled)
+            {
+                _siteService = MrCMSApplication.Get<ISiteService>();
+                _configurationProvider = MrCMSApplication.Get<IConfigurationProvider>();
+            }
+        }
+
+    }
+
+    public abstract class MrCMSPage : MrCMSPage<dynamic>
+    {
+    }
+}
