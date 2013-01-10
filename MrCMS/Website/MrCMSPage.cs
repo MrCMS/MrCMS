a7ab4b6d13ec6bab57a6cdada0013d3a81362677
gmstr <gary.m.strong@gmail.com>
2013-01-10 11:08:25 +0000

    Moving RenderZone into page

--- MrCMS/Website/MrCMSPage.cs
+++ MrCMS/Website/MrCMSPage.cs
@@ -1,4 +1,10 @@
-﻿using MrCMS.Services;
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using MrCMS.Entities.Documents.Web;
+using MrCMS.Services;
 using MrCMS.Settings;
 
 namespace MrCMS.Website
@@ -28,6 +34,47 @@ namespace MrCMS.Website
             }
         }
 
+        public MvcHtmlString RenderZone( string areaName)
+        {
+            var page = Model as Webpage;
+
+            if (page != null && page.CurrentLayout != null)
+            {
+                var layout = page.CurrentLayout;
+
+                var layoutArea = layout.GetLayoutAreas().FirstOrDefault(area => area.AreaName == areaName);
+
+                if (layoutArea == null) return MvcHtmlString.Empty;
+
+                var stringBuilder = new StringBuilder();
+                if (MrCMSApplication.CurrentUserIsAdmin)
+                    stringBuilder.AppendFormat("<div data-layout-area-id=\"{0}\" class=\"layout-area\"> ", layoutArea.Id);
+
+                foreach (var widget in layoutArea.GetWidgets(page))
+                {
+                    if (MrCMSApplication.CurrentUserIsAdmin)
+                        stringBuilder.AppendFormat("<div data-widget-id=\"{0}\" class=\"widget\"> ", widget.Id);
+
+                    try
+                    {
+                        stringBuilder.Append(Html.Action("Show", "Widget", new { id = widget.Id, page }).ToHtmlString());
+                    }
+                    catch (Exception ex)
+                    {
+                        Elmah.ErrorSignal.FromCurrentContext().Raise(ex);
+                    }
+
+                    if (MrCMSApplication.CurrentUserIsAdmin)
+                        stringBuilder.Append("</div>");
+                }
+
+                if (MrCMSApplication.CurrentUserIsAdmin)
+                    stringBuilder.Append("</div>");
+
+                return MvcHtmlString.Create(stringBuilder.ToString());
+            }
+            return MvcHtmlString.Empty;
+        }
     }
 
     public abstract class MrCMSPage : MrCMSPage<dynamic>
