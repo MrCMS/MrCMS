1ac4b27c4c8315a09b31109a2129065ce7f37c6d
gmstr <gary.m.strong@gmail.com>
2013-01-17 13:05:45 +0000

    Updating in-line editing renderer

--- MrCMS/Website/MrCMSPage.cs
+++ MrCMS/Website/MrCMSPage.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Html;
+using System.Web.Routing;
+using MrCMS.Entities;
 using MrCMS.Entities.Documents.Web;
 using MrCMS.Services;
 using MrCMS.Settings;
+using MrCMS.Helpers;
 
 namespace MrCMS.Website
 {
@@ -14,11 +21,13 @@ namespace MrCMS.Website
     {
         private IConfigurationProvider _configurationProvider;
         private ISiteService _siteService;
+        private IImageProcessor _imageProcessor;
 
         public T SiteSettings<T>() where T : SiteSettingsBase, new()
         {
             return _configurationProvider.GetSettings<T>(_siteService.GetCurrentSite());
         }
+
         public T GlobalSettings<T>() where T : GlobalSettingsBase, new()
         {
             return _configurationProvider.GetSettings<T>();
@@ -32,10 +41,50 @@ namespace MrCMS.Website
             {
                 _siteService = MrCMSApplication.Get<ISiteService>();
                 _configurationProvider = MrCMSApplication.Get<IConfigurationProvider>();
+                _imageProcessor = MrCMSApplication.Get<IImageProcessor>();
             }
         }
 
-        public MvcHtmlString RenderZone( string areaName)
+        public MvcHtmlString Editable<T>(T model, Expression<Func<T, string>> method) where T : BaseEntity
+        {
+            if (model == null)
+                return MvcHtmlString.Empty;
+
+            var value = method.Compile().Invoke(model);
+            var typeName = typeof(T).Name;
+            var propertyInfo = PropertyFinder.GetProperty(method);
+
+            if (MrCMSApplication.CurrentUserIsAdmin && propertyInfo != null)
+            {
+                var tagBuilder = new TagBuilder("div");
+                tagBuilder.AddCssClass("editable");
+                tagBuilder.Attributes["data-id"] = model.Id.ToString();
+                tagBuilder.Attributes["data-property"] = propertyInfo.Name;
+                tagBuilder.Attributes["data-type"] = typeName;
+                tagBuilder.Attributes["contenteditable"] = "true";
+                tagBuilder.InnerHtml = value;
+
+                return MvcHtmlString.Create(tagBuilder.ToString());
+            }
+            else
+            {
+                return MvcHtmlString.Create(value);
+            }
+        }
+
+        public class PropertyFinder
+        {
+            public static PropertyInfo GetProperty(Expression expression)
+            {
+                if (expression is MemberExpression && (expression as MemberExpression).Member is PropertyInfo)
+                {
+                    return ((expression as MemberExpression).Member as PropertyInfo);
+                }
+                return null;
+            }
+        }
+
+        public MvcHtmlString RenderZone(string areaName)
         {
             var page = Model as Webpage;
 
@@ -75,6 +124,16 @@ namespace MrCMS.Website
                 return MvcHtmlString.Create(stringBuilder.ToString());
             }
             return MvcHtmlString.Empty;
+        }
+
+        public MvcHtmlString RenderImage(string imageUrl, string alt = null, string title = null, object attributes = null)
+        {
+            return Html.RenderImage(imageUrl, alt, title, attributes);
+        }
+
+        public MvcHtmlString RenderImage(string imageUrl, Size size, string alt = null, string title = null, object attributes = null)
+        {
+            return Html.RenderImage(imageUrl, size, alt, title, attributes);
         }
     }
 
