3e706730679036991cbf886606cc24416f58300b
thought <will.anderson@thought.co.uk>
2013-02-22 15:08:29 +0000

    Front end fixes

--- MrCMS.Web/Areas/Admin/Content/Scripts/front-end-editing.js
+++ MrCMS.Web/Areas/Admin/Content/Scripts/front-end-editing.js
@@ -64,7 +64,8 @@
                         original = el.html();
                     });
                     el.blur(function () {
-                        if (original != el.html()) {
+                        var html = stripHtml(el.html());
+                        if (original != html) {
                             $.ajax({
                                 type: "POST",
                                 url: "/Admin/InPageAdmin/SaveBodyContent",
@@ -72,7 +73,7 @@
                                     id: el.data('id'),
                                     property: el.data('property'),
                                     type: el.data('type'),
-                                    content: el.html()
+                                    content: html
                                 },
                                 success: function (msg) {
                                     showLiveForm(el);
@@ -100,8 +101,8 @@
                 var widgetId = $(this).parent().data('widget-id');
                 var menu = '<div class="edit-widget-menu">' +
                     '<ul>' +
-                    '<li><a id="" href="/Admin/Widget/Edit/' + widgetId + '" target="_parent" class="btn btn-mini" style="color:#333;text-decoration:none;">Edit this widget</a></li>' +
-                    '<li><a id="" data-toggle="fb-modal" href="/Admin/Widget/Delete/' + widgetId + '" target="_parent" class="btn btn-mini" style="color:#333;text-decoration:none;">Delete this widget</a></li>' +
+                    '<li><a id="" href="/Admin/Widget/Edit/' + widgetId + '" target="_parent" class="mrcms-btn mrcms-btn-mini" style="color:#333;text-decoration:none;">Edit this widget</a></li>' +
+                    '<li><a id="" data-toggle="fb-modal" href="/Admin/Widget/Delete/' + widgetId + '" target="_parent" class="mrcms-btn mrcms-btn-mini" style="color:#333;text-decoration:none;">Delete this widget</a></li>' +
                     '</ul></div>';
                 $(this).parent().prepend(menu);
                 $(".edit-widget-menu").fadeIn(400);
@@ -114,15 +115,17 @@
                     }
                 });
             });
-            
+
             //create menu for layout editing
             $('.edit-indicator-layout').click(function () {
                 var areaId = $(this).parent().data('layout-area-id');
                 var areaName = $(this).parent().data('layout-area-name');
+                var pageId = $('#Id').val();
+
                 var menu = '<div class="edit-layout-area-menu"><h4>Layout area: <br /> ' + areaName +
-                    '</h4><ul><li><a tab-index="1" href="/Admin/Widget/AddPageWidget?pageId=' + $('#Id').val() + '&id=' + areaId + '" data-toggle="fb-modal" class="btn btn-mini" style="color:#333;text-decoration:none;">Add new widget here</a></li>' +
-                    '<li><a tab-index="1" href="/Admin/Webpage/Edit/' + $('#Id').val()+ '" class="btn btn-mini" style="color:#333;text-decoration:none;">Manage page widgets</a>' +
-                    '</li><li><a tab-index="1" href="/Admin/LayoutArea/Edit/' + areaId + '" class="btn btn-mini" style="color:#333;text-decoration:none;">Manage global widgets</a></li></ul></div>';
+                    '</h4><ul><li><a tab-index="1" href="/Admin/Widget/AddPageWidget?pageId=' + pageId + '&id=' + areaId + '" data-toggle="fb-modal" class="mrcms-btn mrcms-btn-mini" style="color:#333;text-decoration:none;">Add new widget here</a></li>' +
+                    '<li><a tab-index="1" href="/Admin/Webpage/Edit/' + pageId + '" class="mrcms-btn mrcms-btn-mini" style="color:#333;text-decoration:none;" id="mrcms-manage-page-widgets">Manage page widgets</a>' +
+                    '</li><li><a tab-index="1" href="/Admin/LayoutArea/Edit/' + areaId + '" class="mrcms-btn mrcms-btn-mini" style="color:#333;text-decoration:none;">Manage global widgets</a></li></ul></div>';
 
                 $(this).parent().prepend(menu);
                 $(".edit-layout-area-menu").fadeIn(400);
@@ -133,25 +136,14 @@
                         container.remove();
                     }
                 });
+
+
             });
 
-
-            //$('.edit-indicator').click(function () {
-            //    var areaId = $(this).parent().data('layout-area-id');
-
-            //    var menu = '<div class="admin-tools edit-widget gradient-bg" style="background:#ffffff;"><ul><li><a id="" href="/Admin/Widget/Edit/' + widgetId + '" target="_parent" class="btn btn-mini" style="color:#333;text-decoration:none;">Edit this widget</a></li><li><a href="/Admin/Widget/AddPageWidget?pageId=' + $('#Id').val() + '&layoutAreaId=' + layoutAreaContainerId + '" data-toggle="modal" class="btn btn-mini" style="color:#333;text-decoration:none;">Add new widget here</a></li><li><a href="/Admin/Widget/Delete/' + widgetId + '" data-toggle="modal" class="btn btn-mini" style="color:#333;text-decoration:none;">Delete this widget</a></li></ul></div>';
-
-
-            //    $(this).parent().prepend(menu);
-
-            //    //if click outside hide the menu
-            //    $(document).mouseup(function (e) {
-            //        var container = $(".edit-widget");
-            //        if (container.has(e.target).length === 0) {
-            //            container.hide();
-            //        }
-            //    });
-            //});
+            //set active tab for layout
+            $(document).on('click', '#mrcms-manage-page-widgets', function () {
+                $.cookie('selected-tab-/Admin/Webpage/Edit/' + $('#Id').val(), '#layout-content');
+            });
         },
         disable: function () {
             setEditingEnabled(false);
@@ -168,6 +160,8 @@
             $(settings.editableSelector).attr("contenteditable", "false");
         }
     };
+
+
 
     $.fn.mrcmsinline = function (method) {
         // Method calling logic
@@ -193,6 +187,9 @@
     function setEditingEnabled(value) {
         return $.cookie('mrcms-inline-edit', value, { expires: 1 });
     }
+    function stripHtml(str) {
+        return jQuery('<div />', { html: str }).text();
+    }
 })(jQuery);
 
 $(function () {
@@ -209,8 +206,8 @@ $(function () {
             type: 'iframe',
             padding: 0,
             height: 0,
-            'onComplete': function() {
-                $('#fancybox-frame').load(function() { // wait for frame to load and then gets it's height
+            'onComplete': function () {
+                $('#fancybox-frame').load(function () { // wait for frame to load and then gets it's height
                     $(this).contents().find('form').attr('target', '_parent').css('margin', '0');
                     $('#fancybox-content').height($(this).contents()[0].documentElement.scrollHeight);
                     $.fancybox.center();
