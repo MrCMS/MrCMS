bfd5421a151fe1e2cef3a2c4f7c5d9dc510bbfb8
thought <will.anderson@thought.co.uk>
2013-03-17 19:22:46 +0000

    Added helper link to manage images

---
+++ MrCMS.Web/Apps/Galleries/Areas/Admin/Content/Scripts/gallery.js
@@ -0,0 +1,20 @@
+﻿$("#MediaGallery_Id").change(function (element) {
+    setManageUrl($(this).val());
+});
+
+window.onload = function () {
+    var id = $("#MediaGallery_Id").val();
+
+    if (parseInt(id) > 0) {
+        setManageUrl($("#MediaGallery_Id").val());
+    } else {
+        $("#manage-media").html('');
+    }
+};
+
+function setManageUrl(id) {
+    var mediaCategoryManageUrl = "/Admin/MediaCategory/Edit/" + id;
+    var ahref = "<a href='" + mediaCategoryManageUrl + "'>Manage images</a>";
+    $("#manage-media").html('');
+    $("#manage-media").append(ahref);
+}
