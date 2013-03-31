a667c25a1c1f3ced0191ec104d583be55f11e3bf
thought <will.anderson@thought.co.uk>
2013-03-31 20:50:03 +0100

    Layout Area UI enhancements

--- MrCMS/Entities/Documents/Layout/LayoutArea.cs
+++ MrCMS/Entities/Documents/Layout/LayoutArea.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using MrCMS.Entities.Documents.Web;
 using MrCMS.Helpers;
@@ -8,6 +9,8 @@ namespace MrCMS.Entities.Documents.Layout
     public class LayoutArea : SiteEntity
     {
         public virtual Layout Layout { get; set; } //which layout does the area belong to?
+        [Required]
+        [StringLength(250, ErrorMessage = "Area name max length 250")]
         public virtual string AreaName { get; set; } // IE. Area.Top, Area.Sidebar.First
 
         protected internal virtual IList<Widget.Widget> Widgets { get; set; }
