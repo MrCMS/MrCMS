eedbe85a9325fcd4edcfaef1608637c1e092376b
gmstr <gary.m.strong@gmail.com>
2013-04-02 17:16:38 +0100

    Fixing Tag saving

--- MrCMS/DbConfiguration/Overrides/DocumentOverride.cs
+++ MrCMS/DbConfiguration/Overrides/DocumentOverride.cs
@@ -10,7 +10,7 @@ namespace MrCMS.DbConfiguration.Overrides
         {
             mapping.DiscriminateSubClassesOnColumn("DocumentType");
             mapping.HasMany(x => x.Children).KeyColumn("ParentId");
-            mapping.HasManyToMany(document => document.Tags).Table("DocumentTags");
+            mapping.HasManyToMany(document => document.Tags).Table("DocumentTags").Cascade.SaveUpdate();
             mapping.HasMany(document => document.Versions).KeyColumn("DocumentId").Cascade.All();
             mapping.IgnoreProperty(x=>x.UrlSegment);
         }
