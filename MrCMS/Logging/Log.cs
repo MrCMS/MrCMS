5918a384815e0c04d2e81bddb5af948d0935e3a1
gmstr <gary.m.strong@gmail.com>
2013-04-02 17:28:25 +0100

    Splitting into files

--- MrCMS/Logging/Log.cs
+++ MrCMS/Logging/Log.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using Elmah;
-using FluentNHibernate.Automapping;
-using FluentNHibernate.Automapping.Alterations;
-using MrCMS.DbConfiguration.Types;
 using MrCMS.Entities;
 
 namespace MrCMS.Logging
@@ -14,21 +11,5 @@ namespace MrCMS.Logging
         public virtual Error Error { get; set; }
         public virtual string Message { get; set; }
         public virtual string Detail { get; set; }
-    }
-
-    public enum LogEntryType
-    {
-        Error,
-        Audit
-    }
-
-    public class LogEntryOverride : IAutoMappingOverride<Log>
-    {
-        public void Override(AutoMapping<Log> mapping)
-        {
-            mapping.Map(entry => entry.Error).CustomType<BinaryData>().Length(9999);
-            mapping.Map(entry => entry.Message).CustomType<VarcharMax>().Length(4001);
-            mapping.Map(entry => entry.Detail).CustomType<VarcharMax>().Length(4001);
-        }
     }
 }
