cd5f50e2c1dde613c68eb3267ccba273497c3e3a
gmstr <gary.m.strong@gmail.com>
2013-04-02 21:15:26 +0100

    Adding ability to query DocumentType explicitly

--- MrCMS.Tests/Entities/DocumentTests.cs
+++ MrCMS.Tests/Entities/DocumentTests.cs
@@ -6,11 +6,13 @@ using MrCMS.Entities.Documents;
 using MrCMS.Entities.Documents.Web;
 using MrCMS.Tests.Stubs;
 using NHibernate;
+using NHibernate.Transform;
 using Xunit;
+using MrCMS.Helpers;
 
 namespace MrCMS.Tests.Entities
 {
-    public class DocumentTests
+    public class DocumentTests : InMemoryDatabaseTest
     {
         [Fact]
         public void Document_CanDelete_IsTrueWhenNoChildren()
@@ -70,7 +72,7 @@ namespace MrCMS.Tests.Entities
             var doc = new StubDocument();
 
             var child = new StubDocument();
-            doc.SetChildren(new List<Document> { child });
+            doc.SetChildren(new List<Document> {child});
 
             child.OnDeleting(A.Fake<ISession>());
 
@@ -81,17 +83,52 @@ namespace MrCMS.Tests.Entities
         public void Document_GetVersions_ReturnsVersionsInDescendingCreatedOnOrder()
         {
             var document = new StubDocument();
-            var version1 = new DocumentVersion { CreatedOn = DateTime.UtcNow };
-            var version2 = new DocumentVersion { CreatedOn = DateTime.UtcNow.AddDays(1) };
+            var version1 = new DocumentVersion {CreatedOn = DateTime.UtcNow};
+            var version2 = new DocumentVersion {CreatedOn = DateTime.UtcNow.AddDays(1)};
             document.SetVersions(new List<DocumentVersion>
-                                     {
-                                         version1,
-                                         version2
-                                     });
+                {
+                    version1,
+                    version2
+                });
 
             var versionsModel = document.GetVersions(1);
 
             versionsModel.Items.Should().ContainInOrder(new List<DocumentVersion> {version2, version1});
         }
+
+        [Fact]
+        public void Document_Grouping_CanGroupByDocumentType()
+        {
+            var document1 = new StubDocument();
+            var document2 = new StubDocument();
+            var document3 = new BasicMappedWebpage();
+
+            Session.Transact(session =>
+                {
+                    session.Save(document1);
+                    session.Save(document2);
+                    session.Save(document3);
+                });
+
+            DocumentTypeCount alias = null;
+            var list =
+                Session.QueryOver<Document>()
+                       .SelectList(
+                           builder =>
+                           builder.SelectGroup(document => document.DocumentType)
+                                  .WithAlias(() => alias.DocumentType)
+                                  .SelectCount(document => document.Id)
+                                  .WithAlias(() => alias.Count))
+                       .TransformUsing(Transformers.AliasToBean<DocumentTypeCount>())
+                       .List<DocumentTypeCount>();
+
+            list.Should().HaveCount(2);
+            
+        }
+    }
+    public class DocumentTypeCount
+    {
+        public string DocumentType { get; set; }
+        public int Count { get; set; }
     }
 }
