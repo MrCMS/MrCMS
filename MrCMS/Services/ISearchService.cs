95e2c75b31c6c0265e14b1241e8640a1896006f9
thought <will.anderson@thought.co.uk>
2013-05-23 09:28:09 +0100

    Search fix / fuzzy search

--- MrCMS/Services/ISearchService.cs
+++ MrCMS/Services/ISearchService.cs
@@ -11,9 +11,7 @@ using MrCMS.Entities.Documents.Layout;
 using MrCMS.Entities.Documents.Media;
 using MrCMS.Entities.Documents.Web;
 using MrCMS.Entities.Indexes;
-using MrCMS.Helpers;
 using MrCMS.Indexing.Management;
-using MrCMS.Indexing.Querying;
 using MrCMS.Models;
 using MrCMS.Paging;
 using MrCMS.Website;
@@ -29,89 +27,6 @@ namespace MrCMS.Services
         IPagedList<Webpage> SiteSearch(string query, int page = 1, int pageSize = 10);
     }
 
-    public class SearchService : ISearchService
-    {
-        private readonly ISearcher<Webpage, WebpageIndexDefinition> _webpageSearcher;
-        private readonly ISearcher<Document, DocumentIndexDefinition> _documentSearcher;
-
-        public SearchService(ISearcher<Webpage, WebpageIndexDefinition> webpageSearcher, ISearcher<Document, DocumentIndexDefinition> documentSearcher)
-        {
-            _webpageSearcher = webpageSearcher;
-            _documentSearcher = documentSearcher;
-        }
-
-        public IEnumerable<SearchResultModel> SearchDocuments<T>(string searchTerm) where T : Document
-        {
-            return
-                SearchResults<T>(searchTerm).Select
-                    (x => new SearchResultModel
-                    {
-                        DocumentId = x.Id.ToString(),
-                        Name = x.Name,
-                        LastUpdated = x.UpdatedOn.ToShortDateString(),
-                        DocumentType = x.DocumentType.BreakUpString(),
-                        DisplayName = x.Name + " (" + x.DocumentType.BreakUpString() + ")",
-                    });
-        }
-
-        private IPagedList<T> SearchResults<T>(string searchTerm, int? parentId = null, int page = 1, int pageSize = 10) where T : Document
-        {
-            var documentSearchQuery = new DocumentSearchQuery<T>(searchTerm, parentId);
-            var searchResults = _documentSearcher.Search(documentSearchQuery.ToQuery(), page, pageSize);
-            return new StaticPagedList<T>(searchResults.OfType<T>(), searchResults.GetMetaData());
-        }
-
-        public IPagedList<DetailedSearchResultModel> SearchDocumentsDetailed<T>(string searchTerm, int? parentId, int page = 1) where T : Document
-        {
-            var searchResults = SearchResults<T>(searchTerm, parentId, page);
-            return GetDetailedSearchResultModel(searchResults, page);
-        }
-
-        public IPagedList<Webpage> SiteSearch(string query, int page = 1, int pageSize = 10)
-        {
-            var searchQuery = new WebpageSearchQuery(query);
-            return _webpageSearcher.Search(searchQuery.ToQuery(), page, pageSize, searchQuery.GetFilter());
-        }
-
-        private IPagedList<DetailedSearchResultModel> GetDetailedSearchResultModel<T>(IPagedList<T> args, int page) where T : Document
-        {
-            return new StaticPagedList<DetailedSearchResultModel>(
-                args.Select(arg => new DetailedSearchResultModel
-                {
-                    DocumentId = arg.Id.ToString(),
-                    Name = arg.Name,
-                    DisplayName = arg.Name + " (" + arg.DocumentType.BreakUpString() + ")",
-                    LastUpdated = arg.UpdatedOn.ToShortDateString(),
-                    DocumentType = arg.DocumentType.BreakUpString(),
-                    CreatedOn = arg.CreatedOn.ToShortDateString(),
-                    EditUrl =
-                        string.Format("/Admin/{0}/Edit/{1}",
-                                      GetDocumentEditType(arg),
-                                      arg.Id),
-                    ViewUrl =
-                        (arg is Webpage)
-                            ? "/" + (arg as Webpage).LiveUrlSegment
-                            : null,
-                    AddChildUrl = (arg is Webpage) ?
-                        string.Format("/Admin/Webpage/Add/{0}",
-                                      arg.Id) : null,
-                }), args.GetMetaData());
-        }
-
-        private string GetDocumentEditType(Document document)
-        {
-            switch (document.Unproxy().GetType().Name)
-            {
-                case "Layout":
-                    return "Layout";
-                case "MediaCategory":
-                    return "MediaCategory";
-                default:
-                    return "Webpage";
-            }
-        }
-    }
-
     internal class DocumentSearchQuery<T> where T : Document
     {
         private readonly string _searchTerm;
@@ -122,17 +37,26 @@ namespace MrCMS.Services
             _searchTerm = searchTerm;
             _parentId = parentId;
         }
+        
+        private string MakeFuzzy(string keywords)
+        {
+            var split = keywords.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return string.Join(" ", split.Select(s => s + "~"));
+        }
 
         public Query ToQuery()
         {
             var booleanQuery = new BooleanQuery();
             if (!string.IsNullOrWhiteSpace(_searchTerm))
             {
-                var multiPhraseQuery = new MultiPhraseQuery();
-                foreach (var s in _searchTerm.Split(' '))
-                    multiPhraseQuery.Add(new Term(DocumentIndexDefinition.Name.FieldName, s));
-                //booleanQuery.Add(new FuzzyQuery(new Term("name", _searchTerm), 0.3f, 2), Occur.SHOULD)
-                booleanQuery.Add(multiPhraseQuery, Occur.SHOULD);
+                var fuzzySearchTerm = MakeFuzzy(_searchTerm);
+                var q = new MultiFieldQueryParser(Version.LUCENE_30, FieldDefinition.GetFieldNames(
+                                                                          DocumentIndexDefinition.Name,
+                                                                          DocumentIndexDefinition.UrlSegment), new StandardAnalyzer(Version.LUCENE_30));
+                var query = q.Parse(fuzzySearchTerm);
+
+                booleanQuery.Add(query, Occur.MUST);
+
             };
             if (typeof(Webpage).IsAssignableFrom(typeof(T)))
                 booleanQuery.Add(new TermQuery(new Term("type", typeof(T).FullName)), Occur.MUST);
@@ -148,6 +72,12 @@ namespace MrCMS.Services
     }
     internal class WebpageSearchQuery
     {
+        private string MakeFuzzy(string keywords, decimal? fuzzyness = null)
+        {
+            var split = keywords.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return string.Join(" ", split.Select(s => s + "~"+(fuzzyness.HasValue ? fuzzyness : null)));
+        }
+
         private readonly string _searchTerm;
 
         public WebpageSearchQuery(string searchTerm)
@@ -160,6 +90,7 @@ namespace MrCMS.Services
             Query query = new MatchAllDocsQuery();
             if (!string.IsNullOrWhiteSpace(_searchTerm))
             {
+
                 var multiFieldQueryParser = new MultiFieldQueryParser(Version.LUCENE_30,
                                                                       FieldDefinition.GetFieldNames(
                                                                           WebpageIndexDefinition.Name,
@@ -168,7 +99,7 @@ namespace MrCMS.Services
                                                                           WebpageIndexDefinition.MetaKeywords,
                                                                           WebpageIndexDefinition.MetaDescription),
                                                                       new StandardAnalyzer(Version.LUCENE_30));
-                query = multiFieldQueryParser.Parse(_searchTerm + "~");
+                query = multiFieldQueryParser.Parse(MakeFuzzy(_searchTerm, 0.5m));
             }
             var queryString = query.ToString();
             Debug.WriteLine(queryString);
