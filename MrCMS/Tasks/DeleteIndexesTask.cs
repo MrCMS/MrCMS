fdb3bd1f3f1cede7a73738469b1911045f3546b2
gmstr <gary.m.strong@gmail.com>
2013-06-18 11:45:51 +0100

    Moving indexing to tasks

---
+++ MrCMS/Tasks/DeleteIndexesTask.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using MrCMS.Entities;
+using MrCMS.Services;
+
+namespace MrCMS.Tasks
+{
+    public class DeleteIndexesTask : IndexManagementTask
+    {
+        public DeleteIndexesTask(SiteEntity entity)
+            : base(entity)
+        {
+        }
+
+        protected override void ExecuteLogic()
+        {
+            var definitionTypes = GetDefinitionTypes(Entity.GetType());
+            foreach (var indexManagerBase in definitionTypes.Select(IndexService.GetIndexManagerBase))
+                indexManagerBase.Delete(Entity);
+        }
+    }
+}
---
+++ MrCMS/Tasks/InsertIndexesTask.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using MrCMS.Entities;
+using MrCMS.Services;
+
+namespace MrCMS.Tasks
+{
+    public class InsertIndexesTask : IndexManagementTask
+    {
+        public InsertIndexesTask(SiteEntity entity)
+            : base(entity)
+        {
+        }
+
+        protected override void ExecuteLogic()
+        {
+            var definitionTypes = GetDefinitionTypes(Entity.GetType());
+            foreach (var indexManagerBase in definitionTypes.Select(IndexService.GetIndexManagerBase))
+                indexManagerBase.Insert(Session.Get(Entity.GetType(), Entity.Id));
+        }
+    }
+}
