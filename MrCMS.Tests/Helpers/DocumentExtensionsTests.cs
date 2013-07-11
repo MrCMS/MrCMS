d178e1b663f5c2141b556acb7a03f1478e834ab4
gmstr <gary.m.strong@gmail.com>
2013-07-11 21:44:36 +0100

    Adding document extension tests

---
+++ MrCMS.Tests/Helpers/DocumentExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using MrCMS.Entities.Documents;
+using MrCMS.Tests.Stubs;
+using Xunit;
+using MrCMS.Helpers;
+
+namespace MrCMS.Tests.Helpers
+{
+    public class DocumentExtensionsTests
+    {
+        [Fact]
+        public void DocumentExtensions_SetParent_SetsTheParentDocument()
+        {
+            var child = new StubDocument();
+            var parent = new StubDocument();
+
+            child.SetParent(parent);
+
+            child.Parent.Should().Be(parent);
+        }
+
+        [Fact]
+        public void DocumentExtensions_SetParent_AddsTheChildToTheChildrenCollection()
+        {
+            var child = new StubDocument();
+            var parent = new StubDocument();
+
+            child.SetParent(parent);
+
+            parent.Children.Should().Contain(child);
+        }
+
+        [Fact]
+        public void DocumentExtensions_GetVersion_ReturnsNullIfThereIsNoMatchingVersionInCollection()
+        {
+            var doc = new StubDocument();
+
+            doc.GetVersion(1).Should().Be(null);
+        }
+    }
+}
