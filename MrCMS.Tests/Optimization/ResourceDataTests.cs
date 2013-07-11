0fee81c82c5abf22e6665cf539eb5b186c8f50b5
gmstr <gary.m.strong@gmail.com>
2013-07-11 21:54:18 +0100

    Adding resource data tests

---
+++ MrCMS.Tests/Optimization/ResourceDataTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using MrCMS.Website.Optimization;
+using Xunit;
+
+namespace MrCMS.Tests.Optimization
+{
+    public class ResourceDataTests
+    {
+        [Fact]
+        public void ResourceData_Get_AssignsIsRemoteToValue()
+        {
+            var resourceData = ResourceData.Get(true, null);
+
+            resourceData.IsRemote.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ResourceData_Get_AssignsUrlToValue()
+        {
+            var resourceData = ResourceData.Get(true, "url");
+
+            resourceData.Url.Should().Be("url");
+        }
+    }
+}
