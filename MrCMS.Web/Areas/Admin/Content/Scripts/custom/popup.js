66efa938960ef50ecf18ad516682dd311d4eabbe
thought <will.anderson@thought.co.uk>
2013-07-29 18:12:01 +0100

    Adding popup resizer

---
+++ MrCMS.Web/Areas/Admin/Content/Scripts/custom/popup.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function() {
+    setInterval(resizeWindow, 1500);
+});
+
+function resizeWindow() {
+
+    top.$.fancybox.center();
+    $('#fancybox-content, #fancybox-wrap, #fancybox-frame', top.document).height($('html').height());
+}
