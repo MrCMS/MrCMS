c1cb869666ee30bf7b87dba400b620e1daf2e57d
gmstr <gary.m.strong@gmail.com>
2013-08-06 13:40:28 +0100

    Moving classes into their own files

--- MrCMS/Services/MessageTemplateParser.cs
+++ MrCMS/Services/MessageTemplateParser.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Text;
-using MrCMS.Entities;
 using Ninject;
 using Ninject.Parameters;
 
@@ -35,31 +32,6 @@ namespace MrCMS.Services
         {
             var tokenProviders = _kernel.GetAll<ITokenProvider<T>>();
             return tokenProviders.SelectMany(provider => provider.Tokens.Select(pair => pair.Key)).ToList();
-        }
-    }
-
-    public interface ITokenProvider<T>
-    {
-        IDictionary<string, Func<T, string>> Tokens { get; }
-    }
-
-    public class PropertyTokenProvider<T> : ITokenProvider<T>
-    {
-        private IDictionary<string, Func<T, string>> _tokens;
-
-        public IDictionary<string, Func<T, string>> Tokens { get { return _tokens = _tokens ?? GetTokens(); } }
-
-        private IDictionary<string, Func<T, string>> GetTokens()
-        {
-            var propertyInfos = typeof (T).GetProperties().Where(info =>
-                                                                 info.CanRead && !info.GetIndexParameters().Any() && !info.GetGetMethod().IsStatic &&
-                                                                 !(info.PropertyType.IsGenericType &&
-                                                                   info.PropertyType.GetGenericArguments()
-                                                                       .Any( arg => arg.IsSubclassOf(typeof (SystemEntity)))) &&
-                                                                 !info.PropertyType.IsSubclassOf(typeof (SiteEntity)))
-                                          .ToList();
-
-            return propertyInfos.ToDictionary<PropertyInfo, string, Func<T, string>>(propertyInfo => propertyInfo.Name, propertyInfo => (arg => Convert.ToString(propertyInfo.GetValue(arg, null))));
         }
     }
 }
