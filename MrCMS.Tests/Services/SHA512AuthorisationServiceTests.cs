e8cec4cdf1b7174ffda361b6611318c0c80ae485
gmstr <gary.m.strong@gmail.com>
2013-08-12 16:43:18 +0100

    Updating tests

--- MrCMS.Tests/Services/SHA512AuthorisationServiceTests.cs
+++ MrCMS.Tests/Services/SHA512AuthorisationServiceTests.cs
@@ -1,80 +1,65 @@
-﻿using FluentAssertions;
+﻿using FakeItEasy;
+using FluentAssertions;
 using MrCMS.Entities.People;
 using MrCMS.Services;
+using MrCMS.Settings;
 using Xunit;
 
 namespace MrCMS.Tests.Services
 {
-    public class SHA512AuthorisationServiceTests
+    public class AuthorisationServiceTests
     {
-        private static SHA512AuthorisationService GetSHA512AuthorisationService()
+        private readonly IHashAlgorithm _hashAlgorithm;
+        private readonly AuthorisationService _authorisationService;
+
+        public AuthorisationServiceTests()
         {
-            var authorisationService = new SHA512AuthorisationService();
-            return authorisationService;
+            _hashAlgorithm = A.Fake<IHashAlgorithm>();
+            _authorisationService = new AuthorisationService(_hashAlgorithm);
         }
 
         [Fact]
-        public void SHA512AuthorisationService_ValidatePassword_ShouldThrowOnDifferentPasswords()
+        public void AuthorisationService_ValidatePassword_ShouldThrowOnDifferentPasswords()
         {
-            var authorisationService = GetSHA512AuthorisationService();
-
-            authorisationService.Invoking(service => service.ValidatePassword("password", "differentpassword")).
+            _authorisationService.Invoking(service => service.ValidatePassword("password", "differentpassword")).
                 ShouldThrow<InvalidPasswordException>();
         }
 
         [Fact]
-        public void SHA512AuthorisationService_ValidatePassword_ReturnsTrueIfPasswordsAreTheSame()
+        public void AuthorisationService_ValidatePassword_ReturnsTrueIfPasswordsAreTheSame()
         {
-            var authorisationService = GetSHA512AuthorisationService();
-
-            authorisationService.Invoking(service => service.ValidatePassword("password", "password")).ShouldNotThrow();
+            _authorisationService.Invoking(service => service.ValidatePassword("password", "password")).ShouldNotThrow();
         }
 
         [Fact]
-        public void SHA512AuthorisationService_SetPassword_WithInvalidPasswordShouldThrowException()
+        public void AuthorisationService_SetPassword_WithInvalidPasswordShouldThrowException()
         {
-            var authorisationService = GetSHA512AuthorisationService();
-
             var user = new User();
 
-            authorisationService.Invoking(service => service.SetPassword(user, "password", "differentpassword")).
+            _authorisationService.Invoking(service => service.SetPassword(user, "password", "differentpassword")).
                 ShouldThrow<InvalidPasswordException>();
 
         }
 
         [Fact]
-        public void SHA512AuthorisationService_SetPassword_WithSamePasswordsShouldAssignThePasswordSaltAndHashOfTheUser()
+        public void AuthorisationService_SetPassword_WithSamePasswordsShouldAssignThePasswordSaltAndHashOfTheUser()
         {
-            var authorisationService = GetSHA512AuthorisationService();
-
             var user = new User();
+            A.CallTo(() => _hashAlgorithm.ComputeHash(A<byte[]>._)).Returns(new byte[64]);
 
-            authorisationService.SetPassword(user, "password", "password");
+            _authorisationService.SetPassword(user, "password", "password");
 
             user.PasswordHash.Should().NotBeEmpty();
             user.PasswordSalt.Should().NotBeEmpty();
         }
 
         [Fact]
-        public void SHA512AuthorisationService_ValidateUser_WithIncorrectPasswordShouldReturnFalse()
+        public void AuthorisationService_ValidateUser_WithCorrectPasswordShouldReturnTrue()
         {
-            var authorisationService = GetSHA512AuthorisationService();
             var user = new User();
-            authorisationService.SetPassword(user, "password", "password");
+            _authorisationService.SetPassword(user, "password", "password");
 
-            var result = authorisationService.ValidateUser(user, "incorrectPassword");
-
-            result.Should().BeFalse();
-        }
-
-        [Fact]
-        public void SHA512AuthorisationService_ValidateUser_WithCorrectPasswordShouldReturnTrue()
-        {
-            var authorisationService = GetSHA512AuthorisationService();
-            var user = new User();
-            authorisationService.SetPassword(user, "password", "password");
-
-            var result = authorisationService.ValidateUser(user, "password");
+            var result = _authorisationService.ValidateUser(user, "password");
 
             result.Should().BeTrue();
         }
