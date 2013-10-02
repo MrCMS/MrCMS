f5683fbf65d838bab979c516d6e546ccd7dae325
gmstr <gary.m.strong@gmail.com>
2013-10-02 16:27:26 +0100

    Preventing duplicate indices

--- MrCMS/DbConfiguration/Conventions/ReferencesConvention.cs
+++ MrCMS/DbConfiguration/Conventions/ReferencesConvention.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using FluentNHibernate.Conventions;
 using FluentNHibernate.Conventions.Instances;
 
@@ -5,6 +6,7 @@ namespace MrCMS.DbConfiguration.Conventions
 {
     public class ReferencesConvention : IReferenceConvention
     {
+        private static readonly List<string> Indices = new List<string>();
         public void Apply(IManyToOneInstance instance)
         {
             instance.Cascade.SaveUpdate();
@@ -14,7 +16,10 @@ namespace MrCMS.DbConfiguration.Conventions
                 (instance.EntityType != null) ? instance.EntityType.Name : instance.Name, instance.Property.Name));
 
 
-            instance.Index(string.Format("IX_{0}_{1}", instance.EntityType.Name, instance.Property.Name));
+            var index = string.Format("IX_{0}_{1}", instance.EntityType.Name, instance.Property.Name);
+            if (Indices.Contains(index)) return;
+            instance.Index(index);
+            Indices.Add(index);
         
         }
     }
