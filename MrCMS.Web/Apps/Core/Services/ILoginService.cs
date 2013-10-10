25563bb5f5c87d00c7d1821c8de80535515866de
thought <will.anderson@thought.co.uk>
2013-10-10 20:02:10 +0100

    Adding IOnLoggedIn

--- MrCMS.Web/Apps/Core/Services/ILoginService.cs
+++ MrCMS.Web/Apps/Core/Services/ILoginService.cs
@@ -1,6 +1,7 @@
 using MrCMS.Entities.People;
 using MrCMS.Services;
 using MrCMS.Web.Apps.Core.Models;
+using MrCMS.Website;
 
 namespace MrCMS.Web.Apps.Core.Services
 {
@@ -14,12 +15,14 @@ namespace MrCMS.Web.Apps.Core.Services
         private readonly IUserService _userService;
         private readonly IAuthorisationService _authorisationService;
         private readonly IPasswordManagementService _passwordManagementService;
+        private readonly IUserEventService _userEventService;
 
-        public LoginService(IUserService userService, IAuthorisationService authorisationService, IPasswordManagementService passwordManagementService)
+        public LoginService(IUserService userService, IAuthorisationService authorisationService, IPasswordManagementService passwordManagementService, IUserEventService userEventService)
         {
             _userService = userService;
             _authorisationService = authorisationService;
             _passwordManagementService = passwordManagementService;
+            _userEventService = userEventService;
         }
 
         public LoginResult AuthenticateUser(LoginModel loginModel)
@@ -30,14 +33,17 @@ namespace MrCMS.Web.Apps.Core.Services
             {
                 if (_passwordManagementService.ValidateUser(user, loginModel.Password))
                 {
+                    var guid = CurrentRequestData.UserGuid;
                     _authorisationService.SetAuthCookie(loginModel.Email, loginModel.RememberMe);
+                    CurrentRequestData.CurrentUser = user;
+                    _userEventService.OnUserLoggedIn(user, guid);
                     return user.IsAdmin
-                               ? new LoginResult {Success = true, RedirectUrl = loginModel.ReturnUrl ?? "~/admin"}
-                               : new LoginResult {Success = true, RedirectUrl = loginModel.ReturnUrl ?? "~/"};
+                               ? new LoginResult { Success = true, RedirectUrl = loginModel.ReturnUrl ?? "~/admin" }
+                               : new LoginResult { Success = true, RedirectUrl = loginModel.ReturnUrl ?? "~/" };
                 }
                 message = "Incorrect email or password.";
             }
-            return new LoginResult {Success = false, Message = message};
+            return new LoginResult { Success = false, Message = message };
         }
     }
 }
--- MrCMS.Web/Apps/Core/Services/IUserEventService.cs
+++ MrCMS.Web/Apps/Core/Services/IUserEventService.cs
@@ -1,3 +1,4 @@
+using System;
 using MrCMS.Entities.People;
 
 namespace MrCMS.Web.Apps.Core.Services
@@ -5,5 +6,6 @@ namespace MrCMS.Web.Apps.Core.Services
     public interface IUserEventService
     {
         void OnUserRegistered(User user);
+        void OnUserLoggedIn(User user, Guid previousSession);
     }
 }
