99694e6754834ebb057e5c6542e7a1e5fababbea
gmstr <gary.m.strong@gmail.com>
2013-10-21 13:10:18 +0100

    Adding site into message parser to allow it to work on multi-tenant sites

--- MrCMS/Services/MessageParser.cs
+++ MrCMS/Services/MessageParser.cs
@@ -1,4 +1,5 @@
 ﻿using MrCMS.Entities.Messaging;
+using MrCMS.Entities.Multisite;
 using NHibernate;
 using MrCMS.Helpers;
 
@@ -7,32 +8,34 @@ namespace MrCMS.Services
     public class MessageParser<T, T2> : IMessageParser<T, T2> where T : MessageTemplate, IMessageTemplate<T2>
     {
         private readonly IMessageTemplateParser _messageTemplateParser;
+        private readonly Site _site;
         private readonly ISession _session;
 
-        public MessageParser(IMessageTemplateParser messageTemplateParser, ISession session)
+        public MessageParser(IMessageTemplateParser messageTemplateParser, Site site, ISession session)
         {
             _messageTemplateParser = messageTemplateParser;
+            _site = site;
             _session = session;
         }
 
         public QueuedMessage GetMessage(T2 obj, string fromAddress = null, string fromName = null, string toAddress = null, string toName = null, string cc = null, string bcc = null)
         {
-            var template = _session.QueryOver<T>().Cacheable().SingleOrDefault();
+            var template = _session.QueryOver<T>().Where(arg => arg.Site == _site).Cacheable().SingleOrDefault();
             if (template == null)
                 return null;
 
             return new QueuedMessage
-                {
-                    FromAddress = _messageTemplateParser.Parse(fromAddress ?? template.FromAddress, obj),
-                    FromName = _messageTemplateParser.Parse(fromName ?? template.FromName, obj),
-                    ToAddress = _messageTemplateParser.Parse(toAddress ?? template.ToAddress, obj),
-                    ToName = _messageTemplateParser.Parse(toName ?? template.ToName, obj),
-                    Cc = _messageTemplateParser.Parse(cc ?? template.Cc, obj),
-                    Bcc = _messageTemplateParser.Parse(bcc ?? template.Bcc, obj),
-                    Subject = _messageTemplateParser.Parse(template.Subject, obj),
-                    Body = _messageTemplateParser.Parse(template.Body, obj),
-                    IsHtml = template.IsHtml
-                };
+            {
+                FromAddress = _messageTemplateParser.Parse(fromAddress ?? template.FromAddress, obj),
+                FromName = _messageTemplateParser.Parse(fromName ?? template.FromName, obj),
+                ToAddress = _messageTemplateParser.Parse(toAddress ?? template.ToAddress, obj),
+                ToName = _messageTemplateParser.Parse(toName ?? template.ToName, obj),
+                Cc = _messageTemplateParser.Parse(cc ?? template.Cc, obj),
+                Bcc = _messageTemplateParser.Parse(bcc ?? template.Bcc, obj),
+                Subject = _messageTemplateParser.Parse(template.Subject, obj),
+                Body = _messageTemplateParser.Parse(template.Body, obj),
+                IsHtml = template.IsHtml
+            };
         }
 
         public void QueueMessage(QueuedMessage queuedMessage)
