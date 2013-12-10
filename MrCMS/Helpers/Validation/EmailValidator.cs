35422babb150cdda21e62d6ea072454f42178ec6
thought <will.anderson@thought.co.uk>
2013-12-10 15:10:39 +0000

    adding in MVC Fool Proof and Microsoft MVC validation files.
    adding in core validation attributes for currency and email validation

---
+++ MrCMS/Helpers/Validation/EmailValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace MrCMS.Helpers.Validation
+{
+    /// <summary>
+    /// validate email in something@somthing.something with a maximum TLD of 6 characters.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public class EmailValidator : RegularExpressionAttribute
+    {
+        private const string pattern = @"^\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}\b$";
+
+        static EmailValidator()
+        {
+            // necessary to enable client side validation
+            DataAnnotationsModelValidatorProvider.RegisterAdapter(typeof(EmailValidator), typeof(RegularExpressionAttributeAdapter));
+        }
+
+        public EmailValidator()
+            : base(pattern)
+        {
+            ErrorMessage = "Please enter a valid email.";
+        }
+    }
+
+}
