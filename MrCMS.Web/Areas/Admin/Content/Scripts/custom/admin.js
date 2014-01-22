835fbe17623635ca5c4945b817f8c1b98b06114f
thought <will.anderson@thought.co.uk>
2014-01-22 12:16:22 +0000

    remove un-used javascript from admin.js

--- MrCMS.Web/Areas/Admin/Content/Scripts/custom/admin.js
+++ MrCMS.Web/Areas/Admin/Content/Scripts/custom/admin.js
@@ -5,39 +5,23 @@
     $("#loading").ajaxStop(function () {
         $(this).hide();
     });
-    
     $.ajaxSetup({ cache: false });
-    
-    $.validator.methods.number = function (value, element) {
+
+    $.validator.methods.number = function(value, element) {
         return this.optional(element) ||
             !isNaN(Globalize.parseFloat(value));
-    }
+    };
 
-    $.validator.methods.date = function (value, element) {
+    $.validator.methods.date = function(value, element) {
         return this.optional(element) ||
             !isNaN(Globalize.parseDate(value));
-    }
+    };
     Globalize.culture($("#UICulture").val());
     
     $().dropdown();
     $("[rel='tooltip']").tooltip();
-
-
-    //var currentTab = $.cookie('selected-tab-' + location.pathname);
-    //if (location.hash !== '') {
-    //    $('.inner-content a[href="' + location.hash + '"]').tab('show');
-    //} else if (currentTab) {
-    //    $('.inner-content a[href="' + currentTab + '"]').tab('show');
-    //} else if ($('.inner-content li a[data-toggle="tab"]').length > 0 && $('.inner-content li.active a[data-toggle="tab"]').length == 0) {
-    //    $('.inner-content a[data-toggle="tab"]').eq(0).click();
-    //}
-
-    $('.inner-content a[data-toggle="tab"]').on('shown', function (e) {
-        $.cookie('selected-tab-' + location.pathname, e.target.hash, { expires: 1, path: "/" });
-    });
-
+    
     $(".datepicker").datepicker();
-
 
     $(document).on('click', '.date-time-picker', function () {
         var that = $(this);
@@ -83,11 +67,6 @@
         }
     });
 
-    $(window).resize(function () {
-        $('.modal').each(function (index, element) {
-            resizeModal($(element));
-        });
-    });
     
     $(document).on('click', 'div[data-paging-type="async"] .pagination a[href]', function () {
         var self = $(this);
@@ -106,41 +85,23 @@
         return false;
     });
 
-    $(document).on('click', 'a.more-link', function () {
-        return false;
-    });
-
     $(document).on('change', '#admin-site-selector', function () {
         location.href = $(this).val();
     });
 
 });
 
-function resizeModal(jqElement) {
-    var modal = jqElement.hasClass('modal') ? jqElement : jqElement.parents('.modal');
-    var height = modal.outerHeight(),
-	    windowHeight = $(window).outerHeight(),
-	    width = modal.outerWidth(),
-	    windowWidth = $(window).outerWidth();
-    var top = (windowHeight - height) / 2,
-	    left = (windowWidth - width) / 2;
-
-    modal.css('top', top).css('left', left);
-}
-
+// used in menu for delete
 function getRemoteModel(href) {
     var link = $("<a>");
     link.attr('href', href);
     link.fancybox({
         type: 'iframe',
+        autoSize: true,
+        minHeight: 200,
         padding: 0,
-        height: 0,
-        'onComplete': function() {
-            $('#fancybox-frame').load(function() { // wait for frame to load and then gets it's height
-                $(this).contents().find('form').attr('target', '_parent').css('margin', '0');
-                $('#fancybox-content').height($(this).contents()[0].documentElement.scrollHeight);
-                $.fancybox.center();
-            });
+        afterShow: function () {
+            $(".fancybox-iframe").contents().find('form').attr('target', '_parent').css('margin', '0');
         }
     }).click();
 }
@@ -164,7 +125,6 @@ function extractLast(term) {
 
 
 $(function () {
-    CKEDITOR.config.toolbar = 'Basic';
     CKEDITOR.replaceAll('ckedit-enabled');
     CKEDITOR.on('instanceReady', function () { $(window).resize(); });
 });
