5a7ef0f7d2bdf3305a36400103681aecaa8c4721
thought <will.anderson@thought.co.uk>
2014-02-01 17:51:57 +0000

    made seo helper better
    -- no longer allows dashes at start or end
    -- replaces more than 1 dash with just one dash

--- MrCMS/Helpers/SeoHelper.cs
+++ MrCMS/Helpers/SeoHelper.cs
@@ -7,11 +7,12 @@ namespace MrCMS.Helpers
         //returns a neat url, lower case, allows: -_/0-9a-z
         public static string TidyUrl(string url)
         {
-            //Todo: Limit length of url to whatever google says
             if (string.IsNullOrWhiteSpace(url))
                 return "";
             url = url.ToLower().Replace("&", "and").Replace(".", "").Trim();
             url = Regex.Replace(url.ToLower(), @"[^\w-//]+", "-");
+            url = Regex.Replace(url.ToLower(), @"\-+", "-"); //stops having --- in urls
+            url = url.Trim('-'); // removes dash from begining and end of string
             url = url.Trim('/');
             return url;
         }
