e22e4c225a672df3770dfc3f487974a01218a36d
gmstr <gary.m.strong@gmail.com>
2014-02-07 12:56:48 +0000

    Moving services into core

--- MrCMS.Web/Controllers/ExternalLoginController.cs
+++ MrCMS.Web/Controllers/ExternalLoginController.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using MrCMS.Entities.People;
 using MrCMS.Services;
 using MrCMS.Web.Apps.Core.Models;
 using MrCMS.Web.Apps.Core.Pages;
@@ -102,102 +98,6 @@ namespace MrCMS.Web.Controllers
                 }
                 context.HttpContext.GetOwinContext().Authentication.Challenge(properties, LoginProvider);
             }
-        }
-    }
-
-    public interface IExternalLoginService
-    {
-        bool IsLogin(ExternalLoginInfo externalLoginInfo);
-        void Login(ExternalLoginInfo externalLoginInfo, AuthenticateResult authenticateResult);
-        bool UserExists(string authenticateResult);
-        void AssociateLoginToUser(string email, ExternalLoginInfo externalLoginInfo);
-        bool RequiresAdditionalFieldsForRegistration();
-        void CreateUser(string email, ExternalLoginInfo externalLoginInfo);
-        ActionResult RedirectAfterLogin(string email, string returnUrl);
-        string GetEmail(AuthenticateResult authenticateResult);
-    }
-
-    public class ExternalLoginService : IExternalLoginService
-    {
-        private readonly IAuthorisationService _authorisationService;
-        private readonly UserManager<User> _userManager;
-
-        public ExternalLoginService(UserManager<User> userManager, IAuthorisationService authorisationService)
-        {
-            _userManager = userManager;
-            _authorisationService = authorisationService;
-        }
-
-        public bool IsLogin(ExternalLoginInfo externalLoginInfo)
-        {
-            return _userManager.Find(externalLoginInfo.Login) != null;
-        }
-
-        public void Login(ExternalLoginInfo externalLoginInfo, AuthenticateResult authenticateResult)
-        {
-            User user = _userManager.Find(externalLoginInfo.Login);
-            _authorisationService.SetAuthCookie(user, false);
-            _authorisationService.UpdateClaims(user, authenticateResult.Identity.Claims);
-        }
-
-        public bool UserExists(string email)
-        {
-            return _userManager.FindByName(email) != null;
-        }
-
-        public string GetEmail(AuthenticateResult authenticateResult)
-        {
-            if (authenticateResult != null && authenticateResult.Identity != null)
-            {
-                IEnumerable<Claim> claims = authenticateResult.Identity.Claims;
-                if (claims != null)
-                {
-                    Claim emailClaim = claims.FirstOrDefault(claim => claim.Type == ClaimTypes.Email);
-                    if (emailClaim != null)
-                        return emailClaim.Value;
-                }
-            }
-            return null;
-        }
-
-        public void AssociateLoginToUser(string email, ExternalLoginInfo externalLoginInfo)
-        {
-            if (string.IsNullOrWhiteSpace(email))
-                return;
-            var user = _userManager.FindByName(email);
-            if (user == null)
-                return;
-            _userManager.AddLogin(user.OwinId, externalLoginInfo.Login);
-        }
-
-        public bool RequiresAdditionalFieldsForRegistration()
-        {
-            //TODO: wire in framework to allow extra fields to be added
-
-            return false;
-        }
-
-        public void CreateUser(string email, ExternalLoginInfo externalLoginInfo)
-        {
-            var user = new User { Email = email, IsActive = true };
-            _userManager.Create(user);
-            _userManager.AddLogin(user.OwinId, externalLoginInfo.Login);
-        }
-
-        public ActionResult RedirectAfterLogin(string email, string returnUrl)
-        {
-            var user = _userManager.FindByName(email);
-            if (!string.IsNullOrWhiteSpace(returnUrl))
-                return new RedirectResult(returnUrl);
-            return user.IsAdmin ? new RedirectResult("~/admin") : new RedirectResult("~");
-        }
-    }
-
-    public static class ExternalLoginServiceExtensions
-    {
-        public static Task<bool> UserExistsAsync(this IExternalLoginService service, string email)
-        {
-            return Task.Run(() => service.UserExists(email));
         }
     }
 }
---
+++ MrCMS/Services/IExternalLoginService.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+
+namespace MrCMS.Services
+{
+    public interface IExternalLoginService
+    {
+        bool IsLogin(ExternalLoginInfo externalLoginInfo);
+        void Login(ExternalLoginInfo externalLoginInfo, AuthenticateResult authenticateResult);
+        bool UserExists(string authenticateResult);
+        void AssociateLoginToUser(string email, ExternalLoginInfo externalLoginInfo);
+        bool RequiresAdditionalFieldsForRegistration();
+        void CreateUser(string email, ExternalLoginInfo externalLoginInfo);
+        ActionResult RedirectAfterLogin(string email, string returnUrl);
+        string GetEmail(AuthenticateResult authenticateResult);
+    }
+}
