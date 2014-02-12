3ee00e2409fac2b55b1cf4349fedbf3b55576c1f
thought <will.anderson@thought.co.uk>
2014-02-12 22:08:02 +0000

    article list model binder

--- MrCMS.Web/Apps/Articles/Controllers/ArticleController.cs
+++ MrCMS.Web/Apps/Articles/Controllers/ArticleController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MrCMS.Web.Apps.Articles.ModelBinders;
 using MrCMS.Web.Apps.Articles.Models;
 using MrCMS.Web.Apps.Articles.Pages;
 using MrCMS.Web.Apps.Articles.Services;
