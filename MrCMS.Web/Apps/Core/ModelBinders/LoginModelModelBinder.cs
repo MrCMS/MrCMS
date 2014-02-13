bca4c5a7403ad88e33b528a08eea8d45d7c5a709
gmstr <gary.m.strong@gmail.com>
2014-02-13 14:56:44 +0000

    Adding model binder to trim the user input

---
+++ MrCMS.Web/Apps/Core/ModelBinders/LoginModelModelBinder.cs
@@ -0,0 +1,28 @@
+using System.Web.Mvc;
+using MrCMS.Web.Apps.Core.Models;
+using MrCMS.Website.Binders;
+using NHibernate;
+
+namespace MrCMS.Web.Apps.Core.ModelBinders
+{
+    public class LoginModelModelBinder : MrCMSDefaultModelBinder
+    {
+        public LoginModelModelBinder(ISession session)
+            : base(() => session)
+        {
+        }
+
+        public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var bindModel = base.BindModel(controllerContext, bindingContext);
+            if (bindModel is LoginModel)
+            {
+                if (!string.IsNullOrWhiteSpace((bindModel as LoginModel).Email))
+                {
+                    (bindModel as LoginModel).Email = (bindModel as LoginModel).Email.Trim();
+                }
+            }
+            return bindModel;
+        }
+    }
+}
