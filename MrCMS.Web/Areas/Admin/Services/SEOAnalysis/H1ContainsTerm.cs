d4e92f86fcc1132e1eea645c040984393b0c05f6
thought <will.anderson@thought.co.uk>
2014-03-06 22:24:18 +0000

    update ui for SEO

--- MrCMS.Web/Areas/Admin/Services/SEOAnalysis/H1ContainsTerm.cs
+++ MrCMS.Web/Areas/Admin/Services/SEOAnalysis/H1ContainsTerm.cs
@@ -16,18 +16,18 @@ namespace MrCMS.Web.Areas.Admin.Services.SEOAnalysis
             var element = document.GetElement("h1");
             if (element == null)
             {
-                yield return GetFacet("h1 is missing", SEOAnalysisStatus.Error, "The h1 element is missing in the page");
+                yield return GetFacet("H1 is missing", SEOAnalysisStatus.Error, "The H1 element is missing in the page");
                 yield break;
             }
             string h1Text = document.GetElementText("h1");
             if (h1Text.Contains(analysisTerm, StringComparison.OrdinalIgnoreCase))
                 yield return
-                    GetFacet("h1 contains term", SEOAnalysisStatus.Success,
-                        "The h1 element contains '" + analysisTerm + "'");
+                    GetFacet("H1 contains term", SEOAnalysisStatus.Success,
+                        "The H1 element contains '" + analysisTerm + "'");
             else
                 yield return
-                    GetFacet("h1 contains term", SEOAnalysisStatus.Error,
-                        "The h1 element does not contain '" + analysisTerm + "'");
+                    GetFacet("H1 contains term", SEOAnalysisStatus.Error,
+                        "The H1 element does not contain '" + analysisTerm + "'");
         }
     }
 }
