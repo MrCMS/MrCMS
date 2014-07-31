843a38f5c896673fbd1d1814fbbe6dc4c36635e8
gmstr <gary.m.strong@gmail.com>
2014-07-31 14:00:42 +0100

    Adding initial caching

--- MrCMS/Website/Controllers/WidgetController.cs
+++ MrCMS/Website/Controllers/WidgetController.cs
@@ -1,21 +1,33 @@
 using System.Web.Mvc;
+using System.Web.Mvc.Html;
 using MrCMS.Apps;
 using MrCMS.Entities.Widget;
+using MrCMS.Models;
 using MrCMS.Services;
 using MrCMS.Helpers;
+using MrCMS.Services.Caching;
 
 namespace MrCMS.Website.Controllers
 {
     public class WidgetController : MrCMSUIController
     {
         private readonly IWidgetModelService _service;
+        private readonly IHtmlCacheService _htmlCacheService;
 
-        public WidgetController(IWidgetModelService service)
+        public WidgetController(IWidgetModelService service, IHtmlCacheService htmlCacheService)
         {
             _service = service;
+            _htmlCacheService = htmlCacheService;
         }
 
-        public PartialViewResult Show(Widget widget)
+        public ActionResult Show(Widget widget)
+        {
+            CachingInfo cachingInfo = widget.GetCachingInfo();
+            return _htmlCacheService.GetContent(this, cachingInfo,
+                helper => helper.Action("Internal", "Widget", new { widget }));
+        }
+
+        public PartialViewResult Internal(Widget widget)
         {
             if (MrCMSApp.AppWidgets.ContainsKey(widget.Unproxy().GetType()))
                 RouteData.DataTokens["app"] = MrCMSApp.AppWidgets[widget.Unproxy().GetType()];
