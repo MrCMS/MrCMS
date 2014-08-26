482b9804ea5160165564a3cdfb1ba2bcf4ec27bd
WillAnd <will.anderson@thought.co.uk>
2014-08-26 16:49:11 +0100

    adding site live health check

---
+++ MrCMS/HealthChecks/SiteIsLiveCheck.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using MrCMS.Settings;
+
+namespace MrCMS.HealthChecks
+{
+    public class SiteIsLive : HealthCheck
+    {
+        private readonly SiteSettings _siteSettings;
+
+        public SiteIsLive(SiteSettings siteSettings)
+        {
+            _siteSettings = siteSettings;
+        }
+
+        public override string DisplayName
+        {
+            get { return "Site in live mode"; }
+        }
+
+        public override HealthCheckResult PerformCheck()
+        {
+           
+            if (!_siteSettings.SiteIsLive)
+            {
+                return new HealthCheckResult
+                {
+                    Messages = new List<string> { "The current site is not set to live, please change this in site settings." },
+                    OK = false
+                };
+            }
+            return HealthCheckResult.Success;
+        }
+    }
+}
