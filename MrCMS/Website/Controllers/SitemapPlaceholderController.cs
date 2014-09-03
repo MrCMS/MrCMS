365b8d8a02e7db71d682bc9f397e363dc6c14531
gmstr <gary.m.strong@gmail.com>
2014-09-03 12:51:26 +0100

    Adding a site map place holder and fixing bugs in the core Navigation widget

---
+++ MrCMS/Website/Controllers/SitemapPlaceholderController.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using MrCMS.Entities.Documents.Web;
+using NHibernate;
+
+namespace MrCMS.Website.Controllers
+{
+    public class SitemapPlaceholderController : MrCMSUIController
+    {
+        private readonly ISitemapPlaceholderUIService _sitemapPlaceholderUIService;
+
+        public SitemapPlaceholderController(ISitemapPlaceholderUIService sitemapPlaceholderUIService)
+        {
+            _sitemapPlaceholderUIService = sitemapPlaceholderUIService;
+        }
+
+        public RedirectResult Show(SitemapPlaceholder page)
+        {
+            return _sitemapPlaceholderUIService.Redirect(page);
+        }
+    }
+
+    public interface ISitemapPlaceholderUIService
+    {
+        RedirectResult Redirect(SitemapPlaceholder page);
+    }
+
+    public class SitemapPlaceholderUIService : ISitemapPlaceholderUIService
+    {
+        private readonly ISession _session;
+
+        public SitemapPlaceholderUIService(ISession session)
+        {
+            _session = session;
+        }
+
+        public RedirectResult Redirect(SitemapPlaceholder page)
+        {
+            if (page == null)
+                return new RedirectResult("~");
+
+            var now = CurrentRequestData.Now;
+            var child =
+                _session.QueryOver<Webpage>().Where(webpage => webpage.Parent.Id == page.Id && webpage.PublishOn != null && webpage.PublishOn <= now)
+                    .OrderBy(webpage => webpage.DisplayOrder).Asc
+                    .Take(1).Cacheable().List().FirstOrDefault();
+            return child == null
+                ? new RedirectResult("~")
+                : new RedirectResult(string.Format("~/{0}", child.LiveUrlSegment));
+        }
+    }
+}
