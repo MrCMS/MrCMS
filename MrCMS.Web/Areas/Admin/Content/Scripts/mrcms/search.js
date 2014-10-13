021b6430a368372724812344c37981ca7205cfe4
WillAnd <will.anderson@thought.co.uk>
2014-10-13 11:02:15 +0100

    check for null in search.js for popups
    started localisation for form fields

--- MrCMS.Web/Areas/Admin/Content/Scripts/mrcms/search.js
+++ MrCMS.Web/Areas/Admin/Content/Scripts/mrcms/search.js
@@ -14,21 +14,23 @@
             }
         }
     }).data("ui-autocomplete");
-    autoComplete._renderItem = function (ul, item) {
-        var text = item.label;
-        if (item.displayType)
-            text += ' (' + item.displayType + ')';
-        var actionLink = $('<a>').addClass('').attr('href', item.actionUrl).html(text);
-        var li = $('<li>').html(actionLink);
-        return li.appendTo(ul);
-    };
-    autoComplete._renderMenu = function (ul, items) {
-        var that = this;
-        $.each(items, function (index, item) {
-            if (index > 0) {
-                $('<li>').appendTo(ul);
-            }
-            that._renderItemData(ul, item);
-        });
-    };
+    if (autoComplete != null) {
+        autoComplete._renderItem = function (ul, item) {
+            var text = item.label;
+            if (item.displayType)
+                text += ' (' + item.displayType + ')';
+            var actionLink = $('<a>').addClass('').attr('href', item.actionUrl).html(text);
+            var li = $('<li>').html(actionLink);
+            return li.appendTo(ul);
+        };
+        autoComplete._renderMenu = function (ul, items) {
+            var that = this;
+            $.each(items, function (index, item) {
+                if (index > 0) {
+                    $('<li>').appendTo(ul);
+                }
+                that._renderItemData(ul, item);
+            });
+        };
+    }
 });
