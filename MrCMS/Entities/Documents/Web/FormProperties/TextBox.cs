252e087fb8938140444195c1754e0d406e8cb54a
WillAnd <will.anderson@thought.co.uk>
2015-02-11 22:38:30 +0000

    adding placeholder to textbox
    fix with edit on form

--- MrCMS/Entities/Documents/Web/FormProperties/TextBox.cs
+++ MrCMS/Entities/Documents/Web/FormProperties/TextBox.cs
@@ -1,6 +1,10 @@
+using System.ComponentModel;
+
 namespace MrCMS.Entities.Documents.Web.FormProperties
 {
     public class TextBox : FormProperty
     {
+        [DisplayName("Placeholder Text")]
+        public virtual string PlaceHolder { get; set; }
     }
 }
