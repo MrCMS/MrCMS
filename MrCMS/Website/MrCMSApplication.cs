e0bb742c1576b108a1638089cd0ab144673d18e0
gmstr <gary.m.strong@gmail.com>
2015-04-21 13:28:31 +0100

    Updating core version

--- MrCMS/Website/MrCMSApplication.cs
+++ MrCMS/Website/MrCMSApplication.cs
@@ -18,8 +18,8 @@ namespace MrCMS.Website
 {
     public abstract class MrCMSApplication : HttpApplication
     {
-        public const string AssemblyVersion = "0.4.4.0";
-        public const string AssemblyFileVersion = "0.4.4.0";
+        public const string AssemblyVersion = "0.5.0.0";
+        public const string AssemblyFileVersion = "0.5.0.0";
         private const string CachedMissingItemKey = "cached-missing-item";
 
 
