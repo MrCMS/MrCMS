98d9c6b64f05c749ced6324c275291844e7a6287
gmstr <gary.m.strong@gmail.com>
2015-04-23 17:17:17 +0100

    Tidying

--- MrCMS.Web/Areas/Admin/AdminAreaRegistration.cs
+++ MrCMS.Web/Areas/Admin/AdminAreaRegistration.cs
@@ -1,11 +1,4 @@
-﻿using System.Linq;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using MrCMS.Apps;
-using MrCMS.Services;
-using MrCMS.Settings;
-using MrCMS.Website;
-using Ninject;
+﻿using System.Web.Mvc;
 
 namespace MrCMS.Web.Areas.Admin
 {
@@ -19,12 +12,12 @@ namespace MrCMS.Web.Areas.Admin
         public override void RegisterArea(AreaRegistrationContext context)
         {
             context.MapRoute("batch execute", "batch-run/next/{id}",
-                new { controller = "BatchExecution", action = "ExecuteNext" });
+                new {controller = "BatchExecution", action = "ExecuteNext"});
 
             context.MapRoute(
                 "Admin_default1",
                 "Admin/{controller}/{action}/{id}",
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                new {controller = "Home", action = "Index", id = UrlParameter.Optional}
                 );
         }
     }
