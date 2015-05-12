b3151b58edf22272efe99b56b25fc4f58b4a16da
WillAnd <will.anderson@thought.co.uk>
2015-05-12 16:53:37 +0100

    using stateless session in end request

--- MrCMS/Search/AddUniversalSearchTaskInfoExecutor.cs
+++ MrCMS/Search/AddUniversalSearchTaskInfoExecutor.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using MrCMS.DbConfiguration;
 using MrCMS.Entities;
 using MrCMS.Entities.Multisite;
 using MrCMS.Helpers;
@@ -12,31 +11,30 @@ namespace MrCMS.Search
 {
     public class AddUniversalSearchTaskInfoExecutor : ExecuteEndRequestBase<AddUniversalSearchTaskInfo, UniversalSearchIndexData>
     {
-        private readonly ISession _session;
+        private readonly IStatelessSession _session;
 
-        public AddUniversalSearchTaskInfoExecutor(ISession session)
+        public AddUniversalSearchTaskInfoExecutor(IStatelessSession session)
         {
             _session = session;
         }
 
         public override void Execute(IEnumerable<UniversalSearchIndexData> data)
         {
-            using (new SiteFilterDisabler(_session))
+            _session.Transact(session =>
             {
-                _session.Transact(session =>
+                foreach (var indexData in data)
                 {
-                    foreach (var indexData in data)
+                    session.Insert(new QueuedTask
                     {
-                        session.Save(new QueuedTask
-                        {
-                            Data = JsonConvert.SerializeObject(indexData),
-                            Type = typeof(UniversalSearchIndexTask).FullName,
-                            Status = TaskExecutionStatus.Pending,
-                            Site = GetSite(indexData)
-                        });
-                    }
-                });
-            }
+                        Data = JsonConvert.SerializeObject(indexData),
+                        Type = typeof(UniversalSearchIndexTask).FullName,
+                        Status = TaskExecutionStatus.Pending,
+                        Site = GetSite(indexData),
+                        CreatedOn = CurrentRequestData.Now,
+                        UpdatedOn = CurrentRequestData.Now
+                    });
+                }
+            });
         }
 
         private Site GetSite(UniversalSearchIndexData indexData)
