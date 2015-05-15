8fedc3eb8bc7074a847ae88c546c3ae74e3e1ccf
gmstr <gary.m.strong@gmail.com>
2015-05-15 13:23:59 +0100

    Update version

--- MrCMS/Website/MrCMSApplication.cs
+++ MrCMS/Website/MrCMSApplication.cs
@@ -18,8 +18,8 @@ namespace MrCMS.Website
 {
     public abstract class MrCMSApplication : HttpApplication
     {
-        public const string AssemblyVersion = "0.5.0.2";
-        public const string AssemblyFileVersion = "0.5.0.2";
+        public const string AssemblyVersion = "0.5.0.3";
+        public const string AssemblyFileVersion = "0.5.0.3";
         private const string CachedMissingItemKey = "cached-missing-item";
 
 
