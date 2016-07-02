137776a74f87a75ee7c429c368d9ec71bba86f16
Will-Bill <will.anderson@thought.co.uk>
2016-07-02 10:47:10 +0100

    Checking for proxy obejects in default model binder Remove references index creation - leave to users to profile

--- MrCMS/DbConfiguration/Conventions/ReferencesConvention.cs
+++ MrCMS/DbConfiguration/Conventions/ReferencesConvention.cs
@@ -6,20 +6,13 @@ namespace MrCMS.DbConfiguration.Conventions
 {
     public class ReferencesConvention : IReferenceConvention
     {
-        private static readonly List<string> Indices = new List<string>();
         public void Apply(IManyToOneInstance instance)
         {
             instance.Cascade.SaveUpdate();
             instance.NotFound.Ignore();
 
-            instance.ForeignKey(string.Format("FK_{0}_{1}",
-                (instance.EntityType != null) ? instance.EntityType.Name : instance.Name, instance.Property.Name));
-
-
-            var index = string.Format("IX_{0}_{1}", instance.EntityType.Name, instance.Property.Name);
-            if (Indices.Contains(index)) return;
-            instance.Index(index);
-            Indices.Add(index);
+            instance.ForeignKey(
+                $"FK_{instance.EntityType?.Name ?? instance.Name}_{instance.Property.Name}");
         
         }
     }
