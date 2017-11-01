9bdd46ac187afdd331b4fc02e392ae2c392a8ea9
gmstr <gary.m.strong@gmail.com>
2017-11-01 16:19:40 +0000

    Adding login attempt emails

---
+++ MrCMS.Web/Apps/Core/Services/SendLoginAttemptNotificationEmails.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using MrCMS.Entities.Messaging;
+using MrCMS.Entities.People;
+using MrCMS.Events;
+using MrCMS.Services;
+using MrCMS.Settings;
+using MrCMS.Web.Apps.Core.MessageTemplates;
+
+namespace MrCMS.Web.Apps.Core.Services
+{
+    public class SendLoginAttemptNotificationEmails : IOnAdded<LoginAttempt>
+    {
+        private readonly AuthSettings _settings;
+        private readonly IMessageParser<SuccessfulLoginAttemptMessageTemplate, LoginAttempt> _successParser;
+        private readonly IMessageParser<FailedLoginAttemptMessageTemplate, LoginAttempt> _failureParser;
+
+        public SendLoginAttemptNotificationEmails(
+            AuthSettings settings,
+            IMessageParser<SuccessfulLoginAttemptMessageTemplate, LoginAttempt> successParser,
+            IMessageParser<FailedLoginAttemptMessageTemplate, LoginAttempt> failureParser
+        )
+        {
+            _settings = settings;
+            _successParser = successParser;
+            _failureParser = failureParser;
+        }
+        public void Execute(OnAddedArgs<LoginAttempt> args)
+        {
+            if (!_settings.SendLoginNotificationEmails)
+                return;
+
+            var loginAttempt = args.Item;
+            QueuedMessage message;
+            switch (loginAttempt.Status)
+            {
+                case LoginAttemptStatus.Failure:
+                    if (loginAttempt.User != null)
+                    {
+                        message = _failureParser.GetMessage(loginAttempt);
+                        _failureParser.QueueMessage(message);
+                    }
+                    break;
+                case LoginAttemptStatus.Success:
+                    message = _successParser.GetMessage(loginAttempt);
+                    _successParser.QueueMessage(message);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
--- MrCMS/Settings/AuthSettings.cs
+++ MrCMS/Settings/AuthSettings.cs
@@ -7,6 +7,9 @@ namespace MrCMS.Settings
         [DisplayName("Log Login Attempts"), AppSettingName("log-login-attempts")]
         public bool LogLoginAttempts { get; set; } = true;
 
+        [DisplayName("Send Login Notification Emails"), AppSettingName("send-login-notification-emails")]
+        public bool SendLoginNotificationEmails { get; set; }
+
         [DisplayName("Two Factor Auth Enabled"), AppSettingName("2fa-enabled")]
         public bool TwoFactorAuthEnabled { get; set; }
 
