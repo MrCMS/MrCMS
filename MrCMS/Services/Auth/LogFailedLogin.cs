8a8347a87232808af2429a57e04666eacc443ac4
gmstr <gary.m.strong@gmail.com>
2017-11-02 09:11:17 +0000

    Fix broken tests

--- MrCMS/Services/Auth/LogFailedLogin.cs
+++ MrCMS/Services/Auth/LogFailedLogin.cs
@@ -2,6 +2,7 @@
 using MrCMS.Entities.People;
 using MrCMS.Helpers;
 using MrCMS.Settings;
+using MrCMS.Website;
 using NHibernate;
 
 namespace MrCMS.Services.Auth
@@ -9,26 +10,25 @@ namespace MrCMS.Services.Auth
     public class LogFailedLogin : IOnFailedLogin
     {
         private readonly AuthSettings _authSettings;
-        private readonly HttpRequestBase _request;
         private readonly ISession _session;
 
-        public LogFailedLogin(AuthSettings authSettings, HttpRequestBase request, ISession session)
+        public LogFailedLogin(AuthSettings authSettings, ISession session)
         {
             _authSettings = authSettings;
-            _request = request;
             _session = session;
         }
         public void Execute(UserFailedLoginEventArgs args)
         {
             if (!_authSettings.LogLoginAttempts)
                 return;
+            var request = CurrentRequestData.CurrentContext.Request;
             var loginAttempt = new LoginAttempt
             {
                 User = args.User,
                 Email = args.Email,
                 Status = LoginAttemptStatus.Failure,
-                IpAddress = _request.GetCurrentIP(),
-                UserAgent = _request.UserAgent
+                IpAddress = request.GetCurrentIP(),
+                UserAgent = request.UserAgent
             };
             _session.Transact(session => session.Save(loginAttempt));
         }
--- MrCMS/Services/Auth/LogSuccessfulLogin.cs
+++ MrCMS/Services/Auth/LogSuccessfulLogin.cs
@@ -2,6 +2,7 @@
 using MrCMS.Entities.People;
 using MrCMS.Helpers;
 using MrCMS.Settings;
+using MrCMS.Website;
 using NHibernate;
 
 namespace MrCMS.Services.Auth
@@ -9,26 +10,25 @@ namespace MrCMS.Services.Auth
     public class LogSuccessfulLogin : IOnUserLoggedIn
     {
         private readonly AuthSettings _authSettings;
-        private readonly HttpRequestBase _request;
         private readonly ISession _session;
 
-        public LogSuccessfulLogin(AuthSettings authSettings, HttpRequestBase request,ISession session)
+        public LogSuccessfulLogin(AuthSettings authSettings, ISession session)
         {
             _authSettings = authSettings;
-            _request = request;
             _session = session;
         }
         public void Execute(UserLoggedInEventArgs args)
         {
             if (!_authSettings.LogLoginAttempts)
                 return;
+            var request = CurrentRequestData.CurrentContext.Request;
             var loginAttempt = new LoginAttempt
             {
                 User = args.User,
                 Email = args.User?.Email,
                 Status = LoginAttemptStatus.Success,
-                IpAddress = _request.GetCurrentIP(),
-                UserAgent = _request.UserAgent
+                IpAddress = request.GetCurrentIP(),
+                UserAgent = request.UserAgent
             };
             _session.Transact(session => session.Save(loginAttempt));
         }
