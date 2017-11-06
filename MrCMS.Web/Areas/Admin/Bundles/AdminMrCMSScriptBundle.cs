14ffe12851366a92e457b57fae9e317d748aa243
gmstr <gary.m.strong@gmail.com>
2017-11-06 13:03:33 +0000

    Removing batch.js

--- MrCMS.Web/Areas/Admin/Bundles/AdminMrCMSScriptBundle.cs
+++ MrCMS.Web/Areas/Admin/Bundles/AdminMrCMSScriptBundle.cs
@@ -19,7 +19,6 @@ namespace MrCMS.Web.Areas.Admin.Bundles
                 yield return "~/Areas/Admin/Content/Scripts/mrcms/admin.js";
                 yield return "~/Areas/Admin/Content/Scripts/mrcms/tagging.js";
                 yield return "~/Areas/Admin/Content/Scripts/mrcms/search.js";
-                yield return "~/Areas/Admin/Content/Scripts/mrcms/batch.js";
                 yield return "~/Areas/Admin/Content/Scripts/mrcms/media-uploader.js";
                 yield return "~/Areas/Admin/Content/Scripts/mrcms/mrcms-media-selector.js";
             }
