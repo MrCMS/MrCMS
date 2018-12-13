807f8939c304cdbdd0ac55d4cf36438ba63aea0e
Gary Strong <gary.m.strong@gmail.com>
2018-12-13 10:44:24 +0000

    Adding merge extension point

--- MrCMS/Services/WebsiteManagement/CompleteMergeBatchJobExecutor.cs
+++ MrCMS/Services/WebsiteManagement/CompleteMergeBatchJobExecutor.cs
@@ -1,20 +1,52 @@
-using System.Threading.Tasks;
 using MrCMS.Batching;
 using MrCMS.Entities.Documents.Web;
 using MrCMS.Helpers;
 using MrCMS.Services.Notifications;
 using NHibernate;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MrCMS.Services.WebsiteManagement
 {
-    public class CompleteMergeBatchJobExecutor: BaseBatchJobExecutor<CompleteMergeBatchJob>
+    public class CompleteMergeBatchJobExecutor : BaseBatchJobExecutor<CompleteMergeBatchJob>
     {
-        private readonly ISession _session;
+        static CompleteMergeBatchJobExecutor()
+        {
+            CompleteMergeTypes = new Dictionary<Type, HashSet<Type>>();
 
-        public CompleteMergeBatchJobExecutor(ISession session,
+            foreach (Type type in TypeHelper.GetAllConcreteMappedClassesAssignableFrom<Webpage>().Where(type => !type.ContainsGenericParameters))
+            {
+                var hashSet = new HashSet<Type>();
+
+                var thisType = type;
+                while (typeof(Webpage).IsAssignableFrom(thisType))
+                {
+                    foreach (var assignType in TypeHelper.GetAllConcreteTypesAssignableFrom(
+                        typeof(OnMergeCompletedBase<>).MakeGenericType(thisType)))
+                    {
+                        hashSet.Add(assignType);
+                    }
+                    thisType = thisType.BaseType;
+
+                }
+
+                CompleteMergeTypes.Add(type, hashSet);
+            }
+
+        }
+
+        private readonly ISession _session;
+        private readonly IKernel _kernel;
+        private static readonly Dictionary<Type, HashSet<Type>> CompleteMergeTypes;
+
+        public CompleteMergeBatchJobExecutor(ISession session, IKernel kernel,
             ISetBatchJobExecutionStatus setBatchJobJobExecutionStatus) : base(setBatchJobJobExecutionStatus)
         {
             _session = session;
+            _kernel = kernel;
         }
 
 
@@ -46,7 +78,25 @@ namespace MrCMS.Services.WebsiteManagement
                     session.Save(urlHistory);
                 });
 
+                ApplyCustomMergeLogic(webpage, mergeInto);
+
                 return BatchJobExecutionResult.Success();
+            }
+        }
+
+        private void ApplyCustomMergeLogic(Webpage webpage, Webpage mergeInto)
+        {
+            var type = webpage.Unproxy().GetType();
+
+            if (!CompleteMergeTypes.ContainsKey(type))
+            {
+                return;
+            }
+
+            foreach (var binderType in CompleteMergeTypes[type].Select(assignViewDataType => _kernel.Get(assignViewDataType)))
+            {
+                var completedBase = binderType as OnMergeCompletedBase;
+                completedBase?.MergeCompleted(webpage, mergeInto);
             }
         }
 
---
+++ MrCMS/Services/WebsiteManagement/OnMergeCompletedBase.cs
@@ -0,0 +1,18 @@
+using MrCMS.Entities.Documents.Web;
+
+namespace MrCMS.Services.WebsiteManagement
+{
+    public abstract class OnMergeCompletedBase
+    {
+        public abstract void MergeCompleted(Webpage @from, Webpage to);
+    }
+
+    public abstract class OnMergeCompletedBase<TWebpage> : OnMergeCompletedBase where TWebpage : Webpage
+    {
+        public abstract void MergeCompleted(TWebpage webpage, Webpage from);
+        public override void MergeCompleted(Webpage @from, Webpage to)
+        {
+            MergeCompleted(@from as TWebpage, to);
+        }
+    }
+}
