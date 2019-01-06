646bcde0dfacd97d57b8ad7cfd5a2790b1288340
Will Anderson <will@mrcms.com>
2019-01-06 17:23:51 +0000

    more css amends

--- src/Apps/MrCMS.Web.Apps.Admin/Models/AddLayoutModel.cs
+++ src/Apps/MrCMS.Web.Apps.Admin/Models/AddLayoutModel.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MrCMS.Web.Apps.Admin.Models
 {
     public class AddLayoutModel
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string UrlSegment { get; set; }
         public int? ParentId { get; set; }
     }
