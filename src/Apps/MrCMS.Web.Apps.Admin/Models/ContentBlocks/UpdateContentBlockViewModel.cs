7ce5a3bc84f9538ac556156674c4eb45844cd7b0
Will Anderson <will@mrcms.com>
2019-01-08 15:38:24 +0000

    continue integration of adminlte

--- src/Apps/MrCMS.Web.Apps.Admin/Models/ContentBlocks/UpdateContentBlockViewModel.cs
+++ src/Apps/MrCMS.Web.Apps.Admin/Models/ContentBlocks/UpdateContentBlockViewModel.cs
@@ -1,8 +1,12 @@
-﻿namespace MrCMS.Web.Apps.Admin.Models.ContentBlocks
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MrCMS.Web.Apps.Admin.Models.ContentBlocks
 {
     public class UpdateContentBlockViewModel
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        public int WebpageId { get; set; }
     }
 }
