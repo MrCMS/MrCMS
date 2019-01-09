e35eca1fe3bd69d31c5584caa9bbb8da390cc526
Will Anderson <will@mrcms.com>
2019-01-09 11:56:23 +0000

    start updating UI libs

--- src/MrCMS.Web/wwwroot/Areas/Admin/lib/jquery/validate/localization/messages_ar.js
+++ src/MrCMS.Web/wwwroot/Areas/Admin/lib/jquery/validate/localization/messages_ar.js
@@ -1,6 +1,8 @@
 (function( factory ) {
 	if ( typeof define === "function" && define.amd ) {
 		define( ["jquery", "../jquery.validate"], factory );
+	} else if (typeof module === "object" && module.exports) {
+		module.exports = factory( require( "jquery" ) );
 	} else {
 		factory( jQuery );
 	}
@@ -10,7 +12,7 @@
  * Translated default messages for the jQuery validation plugin.
  * Locale: AR (Arabic; العربية)
  */
-$.extend($.validator.messages, {
+$.extend( $.validator.messages, {
 	required: "هذا الحقل إلزامي",
 	remote: "يرجى تصحيح هذا الحقل للمتابعة",
 	email: "رجاء إدخال عنوان بريد إلكتروني صحيح",
@@ -22,12 +24,12 @@ $.extend($.validator.messages, {
 	creditcard: "رجاء إدخال رقم بطاقة ائتمان صحيح",
 	equalTo: "رجاء إدخال نفس القيمة",
 	extension: "رجاء إدخال ملف بامتداد موافق عليه",
-	maxlength: $.validator.format("الحد الأقصى لعدد الحروف هو {0}"),
-	minlength: $.validator.format("الحد الأدنى لعدد الحروف هو {0}"),
-	rangelength: $.validator.format("عدد الحروف يجب أن يكون بين {0} و {1}"),
-	range: $.validator.format("رجاء إدخال عدد قيمته بين {0} و {1}"),
-	max: $.validator.format("رجاء إدخال عدد أقل من أو يساوي (0}"),
-	min: $.validator.format("رجاء إدخال عدد أكبر من أو يساوي (0}")
-});
-
+	maxlength: $.validator.format( "الحد الأقصى لعدد الحروف هو {0}" ),
+	minlength: $.validator.format( "الحد الأدنى لعدد الحروف هو {0}" ),
+	rangelength: $.validator.format( "عدد الحروف يجب أن يكون بين {0} و {1}" ),
+	range: $.validator.format( "رجاء إدخال عدد قيمته بين {0} و {1}" ),
+	max: $.validator.format( "رجاء إدخال عدد أقل من أو يساوي {0}" ),
+	min: $.validator.format( "رجاء إدخال عدد أكبر من أو يساوي {0}" )
+} );
+return $;
 }));
--- src/MrCMS.Web/wwwroot/Areas/Admin/lib/jquery/validate/localization/messages_he.js
+++ src/MrCMS.Web/wwwroot/Areas/Admin/lib/jquery/validate/localization/messages_he.js
@@ -1,6 +1,8 @@
 (function( factory ) {
 	if ( typeof define === "function" && define.amd ) {
 		define( ["jquery", "../jquery.validate"], factory );
+	} else if (typeof module === "object" && module.exports) {
+		module.exports = factory( require( "jquery" ) );
 	} else {
 		factory( jQuery );
 	}
@@ -10,7 +12,7 @@
  * Translated default messages for the jQuery validation plugin.
  * Locale: HE (Hebrew; עברית)
  */
-$.extend($.validator.messages, {
+$.extend( $.validator.messages, {
 	required: "השדה הזה הינו שדה חובה",
 	remote: "נא לתקן שדה זה",
 	email: "נא למלא כתובת דוא\"ל חוקית",
@@ -22,12 +24,12 @@ $.extend($.validator.messages, {
 	creditcard: "נא למלא מספר כרטיס אשראי חוקי",
 	equalTo: "נא למלא את אותו ערך שוב",
 	extension: "נא למלא ערך עם סיומת חוקית",
-	maxlength: $.validator.format(".נא לא למלא יותר מ- {0} תווים"),
-	minlength: $.validator.format("נא למלא לפחות {0} תווים"),
-	rangelength: $.validator.format("נא למלא ערך בין {0} ל- {1} תווים"),
-	range: $.validator.format("נא למלא ערך בין {0} ל- {1}"),
-	max: $.validator.format("נא למלא ערך קטן או שווה ל- {0}"),
-	min: $.validator.format("נא למלא ערך גדול או שווה ל- {0}")
-});
-
+	maxlength: $.validator.format( ".נא לא למלא יותר מ- {0} תווים" ),
+	minlength: $.validator.format( "נא למלא לפחות {0} תווים" ),
+	rangelength: $.validator.format( "נא למלא ערך בין {0} ל- {1} תווים" ),
+	range: $.validator.format( "נא למלא ערך בין {0} ל- {1}" ),
+	max: $.validator.format( "נא למלא ערך קטן או שווה ל- {0}" ),
+	min: $.validator.format( "נא למלא ערך גדול או שווה ל- {0}" )
+} );
+return $;
 }));
