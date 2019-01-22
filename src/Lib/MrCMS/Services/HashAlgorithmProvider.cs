f60a11dbc739dc0f2f33b4f19b57f4a4de8326d4
Gary Strong <gary.m.strong@gmail.com>
2019-01-22 09:43:14 +0000

    Allowing hashing method to be readable from config

--- src/Lib/MrCMS/Services/HashAlgorithmProvider.cs
+++ src/Lib/MrCMS/Services/HashAlgorithmProvider.cs
@@ -1,15 +1,18 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Extensions.Configuration;
 
 namespace MrCMS.Services
 {
     public class HashAlgorithmProvider : IHashAlgorithmProvider
     {
+        private readonly IConfiguration _configuration;
         private readonly IDictionary<string, IHashAlgorithm> _hashAlgorithms;
 
-        public HashAlgorithmProvider(IEnumerable<IHashAlgorithm> hashAlgorithms)
+        public HashAlgorithmProvider(IEnumerable<IHashAlgorithm> hashAlgorithms, IConfiguration configuration)
         {
+            _configuration = configuration;
             // for performance, we'll use a dictionary, but for a bit of leniency, we'll use a case-insensitive checker
             _hashAlgorithms = hashAlgorithms.GroupBy(x => x.Type)
                 .ToDictionary(grouping => grouping.Key, grouping => grouping.First(), StringComparer.OrdinalIgnoreCase);
@@ -20,7 +23,7 @@ namespace MrCMS.Services
             // we want to first see if the one they've asked for is there, then check to see if a default has been configured.
             // if both of these are unavailable, we drop back onto the default SHA512 implementation
             return GetAlgorithm(type)
-                   //?? GetAlgorithm(WebConfigurationManager.AppSettings["HashingMethod"]) // TODO: implement settings for hash algorithm
+                   ?? GetAlgorithm(_configuration["HashingMethod"]) 
                    ?? new SHA512HashAlgorithm();
         }
 
