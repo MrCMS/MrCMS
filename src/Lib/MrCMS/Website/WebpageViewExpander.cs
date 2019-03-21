4a94b6e227c449342fbb6cd00c2ba67f73292c96
Gary Strong <gary.m.strong@gmail.com>
2019-03-21 18:55:10 +0000

    Adding Webpage view expansion for Page Templates

--- src/Lib/MrCMS/Website/WebpageViewExpander.cs
+++ src/Lib/MrCMS/Website/WebpageViewExpander.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Microsoft.AspNetCore.Mvc.Razor;
+﻿using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.DependencyInjection;
 using MrCMS.Services;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MrCMS.Website
 {
@@ -12,19 +12,36 @@ namespace MrCMS.Website
 
         public void PopulateValues(ViewLocationExpanderContext context)
         {
-            if (!context.IsMainPage) return;
+            if (!context.IsMainPage)
+            {
+                return;
+            }
+
             var webpage = context.ActionContext.HttpContext.RequestServices.GetRequiredService<IGetCurrentPage>().GetPage();
 
             if (webpage != null)
+            {
                 context.Values[Key] = webpage.DocumentType;
+            }
         }
 
         public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
         {
-            if (!context.Values.ContainsKey(Key))
-                return viewLocations;
+            if (context.Values.ContainsKey(Key))
+            {
+                // we want to look in the pages folder for the document type
+                viewLocations = viewLocations.Prepend($"/Views/Pages/{context.Values[Key]}.cshtml");
 
-            return viewLocations.Prepend($"/Views/Pages/{context.Values[Key]}.cshtml");
+                // if we've set the view name, we want to look here for the view
+                // it's after the base on because we're prepending
+                if (!string.IsNullOrWhiteSpace(context.ViewName))
+                {
+                    viewLocations = viewLocations.Prepend($"/Views/Pages/{context.ViewName}.cshtml");
+                }
+            }
+
+            return viewLocations;
+
         }
     }
 }
