4df89acf4e5a5e2c74565d4182bcd3c940107e07
Dalir <dalir.bajelani@thought.co.uk>
2025-05-28 14:09:57 +0100

    Tightens referrer policy for enhanced security
    Updates the referrer policy to `same-origin`.
    
    This enhances security by ensuring that the full URL is only sent as a referrer when navigating within the same origin, preventing sensitive data leakage to other domains.

--- src/MrCMS.Web/Startup.cs
+++ src/MrCMS.Web/Startup.cs
@@ -263,7 +263,7 @@ namespace MrCMS.Web
                 context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
                 context.Response.Headers.Add("X-Xss-Protection", "1; mode=block");
                 context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
-                context.Response.Headers.Add("Referrer-Policy", "origin");
+                context.Response.Headers.Add("Referrer-Policy", "same-origin");
                 context.Response.Headers.Add("X-Permitted-Cross-Domain-Policies", "none");
                 await next();
             });
